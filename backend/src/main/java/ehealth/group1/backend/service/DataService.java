b7c21a1701afa95351ea2c00d6639cab102b4707
SyntheticDoc <e0604573@student.tuwien.ac.at>
2023-04-13 18:39:32 +0200

    Implented CommandLineRunner-Bean, FhirContext-Bean in FhirContextConfig, DataService with FhirContext constructor-injection
    Signed-off-by: SyntheticDoc <e0604573@student.tuwien.ac.at>

--- backend/src/main/java/ehealth/group1/backend/service/DataService.java
+++ backend/src/main/java/ehealth/group1/backend/service/DataService.java
@@ -1,7 +1,21 @@
 package ehealth.group1.backend.service;
 
+import ca.uhn.fhir.context.FhirContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.lang.invoke.MethodHandles;
 
 @Component
 public class DataService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final FhirContext ctx;
+
+    @Autowired
+    public DataService(FhirContext ctx) {
+        this.ctx = ctx;
+    }
 }

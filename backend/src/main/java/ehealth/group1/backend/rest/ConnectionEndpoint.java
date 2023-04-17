c42808259d1af29ba813994fd38fde2d22c66a72
SyntheticDoc <e0604573@student.tuwien.ac.at>
2023-04-17 15:59:25 +0200

    Added endpoint classes
    Signed-off-by: SyntheticDoc <e0604573@student.tuwien.ac.at>

---
+++ backend/src/main/java/ehealth/group1/backend/rest/ConnectionEndpoint.java
@@ -0,0 +1,14 @@
+package ehealth.group1.backend.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.invoke.MethodHandles;
+
+@RestController
+public class ConnectionEndpoint {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+
+}
--- backend/src/main/java/ehealth/group1/backend/rest/DataEndpoint.java
+++ backend/src/main/java/ehealth/group1/backend/rest/DataEndpoint.java
@@ -1,14 +1,20 @@
 package ehealth.group1.backend.rest;
 
 import ehealth.group1.backend.service.ECGService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.invoke.MethodHandles;
+
 @RestController
 public class DataEndpoint {
+  private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
   ECGService ecgService;
+
   public DataEndpoint(ECGService ecgService){
     this.ecgService = ecgService;
   }

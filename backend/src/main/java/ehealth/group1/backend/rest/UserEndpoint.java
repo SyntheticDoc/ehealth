141c1204e6311cfc11212fd3444e4ab96b1f5feb
SyntheticDoc <e0604573@student.tuwien.ac.at>
2023-04-19 22:15:23 +0200

    Moved two methods from DataEndpoint to UserEndpoint
    Signed-off-by: SyntheticDoc <e0604573@student.tuwien.ac.at>

--- backend/src/main/java/ehealth/group1/backend/rest/UserEndpoint.java
+++ backend/src/main/java/ehealth/group1/backend/rest/UserEndpoint.java
@@ -1,14 +1,32 @@
 package ehealth.group1.backend.rest;
 
+import ehealth.group1.backend.service.ECGService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.invoke.MethodHandles;
 
 @RestController
+@RequestMapping(path = "/user")
 public class UserEndpoint {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    ECGService ecgService;
+
+    public UserEndpoint(ECGService ecgService){
+        this.ecgService = ecgService;
+    }
+
+    @GetMapping("/")
+    public int returnHealthStatus(){
+        return 1;
+    }
+
+    @PostMapping("/getRecentEcg")
+    public String getRecentEcgData(@RequestBody String data){
+        LOGGER.info("/data called?");
+        return ecgService.getData(data);
+    }
 
 }

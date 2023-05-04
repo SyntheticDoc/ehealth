5291fafbecaecd528d79d84ce30d24b7ee494b83
SyntheticDoc <e0604573@student.tuwien.ac.at>
2023-05-04 10:08:06 +0200

    Deleted tests unnecessary for jely
    Signed-off-by: SyntheticDoc <e0604573@student.tuwien.ac.at>

--- backend/src/main/java/ehealth/group1/backend/BackendApplication.java
+++ backend/src/main/java/ehealth/group1/backend/BackendApplication.java
@@ -59,105 +59,11 @@ public class BackendApplication {
           switch (arg) {
             case "test":
               testDataLoader.exec();
-              //execHexIDTests();
-              execTests();
               jelyTest();
           }
         }
       }
     };
-  }
-
-  private void execHexIDTests() {
-    for(int i = 0; i < 5; i++) {
-      String hexString = IDStringGenerator.getNewIDString();
-      LOGGER.warn("Generated hex id: " + hexString);
-    }
-  }
-
-/*
-  private void execDBTests2() {
-    User user = new User(57L, "NewUser", "ExampleAddress", 42L, true, "pwd");
-    LOGGER.info("Creating new user: " + user);
-
-    User newUser = userRepository.save(user);
-
-    LOGGER.info("User created: " + userRepository.findById(newUser.getId()));
-
-
-    User user2 = new User(userId, "NewUser2", "ExampleAddress2", 43L, false, "pwd2");
-
-    dataDao.updateUser(user2);
-
-    LOGGER.info("User updated: " + dataDao.searchUser(user2).get(0));
-
-    User user3 = new User(null, "NewUser2", "ExampleAddress2", 43L, false, "pwd2");
-
-    List<User> searchResult = dataDao.searchUser(user3);
-
-    LOGGER.info("User search result: " + searchResult.get(0));
-    LOGGER.info("Search result list length: " + searchResult.size());
-
-    dataDao.deleteUser(user2);
-
-    LOGGER.info("User exists?: " + dataDao.userExists(user2.getId()));
-  }
-
-  private void execDBTests() {
-    LOGGER.warn("Executing database tests...");
-
-    if(true) {
-      return;
-    }
-
-    Settings settings = settingsRepository.findByUserId(0L);
-    ECGStateHolderSettings stateholder = settings.getEcgStateHolderSettings();
-    ECGAnalysisSettings analysis = settings.getEcgAnalysisSettings();
-
-    User user = userRepository.findById(0L).orElseThrow(() -> new PersistenceException("Error while retrieving user from userRepository"));
-
-    StringBuilder s = new StringBuilder();
-
-    s.append("\n\n");
-    s.append("  Settings - id: ").append(settings.getId()).append("\n");
-    s.append("  Settings - user_id: ").append(settings.getUserId()).append("\n");
-    s.append("  Stateholder - id: ").append(stateholder.getId()).append("\n");
-    s.append("  Stateholder - user_id: ").append(stateholder.getUser_id()).append("\n");
-    s.append("  Stateholder - iterations_transition: ").append(stateholder.getIterationsToStateTransition()).append("\n");
-    s.append("  Stateholder - iterations_emergency: ").append(stateholder.getIterationsToEmergencyCall()).append("\n");
-    s.append("  Analysis - id: ").append(analysis.getId()).append("\n");
-    s.append("  Analysis - user_id: ").append(analysis.getUser_id()).append("\n");
-    s.append("  Analysis - maxDeviations: ").append(analysis.getMaxDeviation()).append("\n");
-    s.append("  Analysis - maxDeviations_num: ").append(analysis.getMaxDeviationNum()).append("\n");
-    s.append("--------------------------------\n");
-    s.append("  User - id: ").append(user.getId()).append("\n");
-    s.append("  User - name: ").append(user.getName()).append("\n");
-    s.append("  User - address: ").append(user.getAddress()).append("\n");
-    s.append("  User - phone: ").append(user.getPhone()).append("\n");
-    s.append("  User - emergency: ").append(user.getEmergency()).append("\n");
-    s.append("  User - password: ").append(user.getPassword()).append("\n");
-
-    LOGGER.info("Data:" + s);
-  }
-
-  private void execTests() {
-    LOGGER.warn("Executing quick startup tests...");
-
-    LOGGER.info("Creating fhir contexts...");
-
-    FhirContext c1 = ctx;
-    FhirContext c2 = FhirContext.forR5Cached();
-    FhirContext c3 = FhirContext.forR5Cached();
-    FhirContext c4 = FhirContext.forR5();
-    FhirContext c5 = FhirContext.forR5Cached();
-
-    LOGGER.info("Context c1: " + c1.hashCode());
-    LOGGER.info("Context c2: " + c2.hashCode());
-    LOGGER.info("Context c3: " + c3.hashCode());
-    LOGGER.info("Context c4: " + c4.hashCode());
-    LOGGER.info("Context c5: " + c5.hashCode());
-
-    LOGGER.info("Startup tests finished.");
   }
 
   private void jelyTest() {
@@ -177,5 +83,5 @@ public class BackendApplication {
       LOGGER.info("\n\nHeartbeat: " + h.toString() + "\nQRS: " + qrs.toString() + "\nR-peak: " + rPeak + "\n\n");
     }
   }
- */
+
 }

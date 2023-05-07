a9cb442b525d7214ba9dacd8ea9585f0bb63e3e4
11906243 <e11906243@student.tuwien.ac.at>
2023-05-07 16:51:32 +0200

    ui

---
+++ frontend/type/User.js
@@ -0,0 +1,15 @@
+/*class User {
+    constructor(name, address, phone, emergency, password) {
+      this.name = name;
+      this.address = address;
+      this.phone = phone;
+      this.emergency = emergency;
+      this.password = password;
+    }
+  }*/
+
+
+  import { createContext } from 'react';
+
+const UserContext = createContext();
+export default UserContext;
